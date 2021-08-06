6731f84fd90b041c974ff78aea6abfe8b0327b53
beginner1986 <beginner1986@gmail.com>
2021-08-06 14:27:44 +0200

    JS code stucturized

--- src/script.js
+++ src/script.js
@@ -1,24 +1,10 @@
 // adding tasks to the list
 let addTaskButton = document.getElementById("add-task-button");
 addTaskButton.addEventListener("click", function () {
-    let checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-
-    let span = document.createElement("span");
-    span.classList.add("task");
-    span.innerHTML = document.querySelector("input").value;
-
-    let deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("delete-btn");
-    deleteBtn.innerHTML = "X";
-    deleteBtn.addEventListener("click", function () {
-        deleteBtn.parentNode.parentNode.removeChild(deleteBtn.parentNode);
-    });
-
     let listItem = document.createElement("li");
-    listItem.appendChild(checkbox);
-    listItem.appendChild(span);
-    listItem.appendChild(deleteBtn);
+    listItem.appendChild(createCheckbox());
+    listItem.appendChild(createSpan());
+    listItem.appendChild(createDeleteBtn());
 
     let taskList = document.getElementById("task-list");
     taskList.appendChild(listItem);
@@ -32,4 +18,30 @@ for(let i = 0; i < deleteBtns.length; i++) {
     deleteBtns[i].addEventListener("click", function () {
         deleteBtns[i].parentNode.parentNode.removeChild(deleteBtns[i].parentNode);
     });
+}
+
+function createCheckbox() {
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+
+    return checkbox;
+}
+
+function createSpan() {
+    let span = document.createElement("span");
+    span.classList.add("task");
+    span.innerHTML = document.querySelector("input").value;
+
+    return span;
+}
+
+function createDeleteBtn() {
+    let deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.innerHTML = "X";
+    deleteBtn.addEventListener("click", function () {
+        deleteBtn.parentNode.parentNode.removeChild(deleteBtn.parentNode);
+    });
+
+    return deleteBtn;
 }
